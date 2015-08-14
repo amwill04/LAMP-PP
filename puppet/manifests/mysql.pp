07a0b6caca670687ebb2df41fdb13389806af576
amwill04 <alanwilliams@Alans-iMac.local>
2015-08-14 13:28:50 +0100

    puppet update

--- puppet/manifests/mysql.pp
+++ puppet/manifests/mysql.pp
@@ -1,6 +1,6 @@
 class mysql {
   exec { 'apt-get mysql-connector':
   command => "sudo apt-get install python-mysql.connector -y",
-  require => Exec['apt-get update']
+  require => Exec['apt-get upgrade']
   }
 }
