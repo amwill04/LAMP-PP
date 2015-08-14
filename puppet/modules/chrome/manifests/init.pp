3338a46c0bec793023f81cd581c912b3eb813179
amwill04 <alanwilliams@Alans-iMac.local>
2015-08-14 17:18:22 +0100

    current bug

--- puppet/modules/chrome/manifests/init.pp
+++ puppet/modules/chrome/manifests/init.pp
@@ -1,8 +1,6 @@
 class chrome {
   file { '/home/vagrant/chrome.sh':
   ensure => present,
-  owner => 'root',
-  group => 'root',
   mode => '0755',
   source => 'puppet:///modules/chrome/chromedriver.sh',
   }
