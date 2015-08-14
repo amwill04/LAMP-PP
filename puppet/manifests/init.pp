dda4200228c11eebc1499464a675a2e721eca495
amwill04 <alanwilliams@Alans-iMac.local>
2015-08-14 13:13:05 +0100

    mysql update

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -2,6 +2,8 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 import "system-update.pp"
 import "python.pp"
+import "mysql.pp"
 
+include mysql
 include pip
 include system-update
---
+++ puppet/manifests/mysql.pp
@@ -0,0 +1,6 @@
+class mysql {
+  exec { 'apt-get mysql-connector':
+  command => "sudo apt-get install python-mysql.connector -y",
+  require => Exec['apt-get update']
+  }
+}
