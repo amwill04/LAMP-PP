66f47c77ae576f1011bacfc7e1526c35eff1620f
amwill04 <alanwilliams@macbook-pro.home>
2015-08-14 21:27:48 +0100

    chromedrive install

--- puppet/modules/chrome/files/chromedriver.sh
+++ puppet/modules/chrome/files/chromedriver.sh
@@ -3,5 +3,7 @@
 cd /tmp
 wget http://chromedriver.storage.googleapis.com/2.16/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
 chmod +x chromedriver
 sudo mv -f chromedriver /usr/local/share/chromedriver
+rm -f /home/vagrant/chromedriver.sh
--- puppet/modules/chrome/manifests/init.pp
+++ puppet/modules/chrome/manifests/init.pp
@@ -1,7 +1,11 @@
 class chrome {
-  file { '/home/vagrant/chrome.sh':
+  file { "/home/vagrant/chromedriver.sh":
   ensure => present,
   mode => '0755',
   source => 'puppet:///modules/chrome/chromedriver.sh',
   }
+
+  exec { "install chromedriver":
+  command => "sudo sh /home/vagrant/chromedriver.sh",
+  require => File["/home/vagrant/chromedriver.sh"]}
 }
